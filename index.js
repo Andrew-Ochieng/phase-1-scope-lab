bef4b48ff82918cd05cd8185151fde0df51ea475
AndrewMartial <andrewomosh44@gmail.com>
2022-08-18 11:04:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+// customerName
+var customerName = 'bob'
+
+// upperCaseCustomerName
+function upperCaseCustomerName () {
+    customerName = customerName.toUpperCase();
+
+}
+
+
+
+// setBestCustomer
+function setBestCustomer () {
+    bestCustomer = 'not bob'
+}
+
+
+// overwriteBestCustomer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+// changesLeastFavouriteCustomer
+const leastFavoriteCustomer = 'joe'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'brandon'
+}
+
+
+
+
+
+
